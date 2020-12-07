16598d7b576d494aa8310db356abf9d9a86bb834
hibahsalam <hibahsalam@yahoo.com>
2020-12-07 10:48:15 +0530

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputComponent } from './input/input.component';
+import { TitleComponent } from './title/title.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    TitleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
